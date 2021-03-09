c2b00bb74efd6e1cfd36b63186ed16828d560c4c
Julio Lopez <joeyclopez88#gmail.com>
2021-03-08 16:33:24 -0800

    first commit

---
+++ I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Julio Lopez on 3/6/21.
+//  Copyright © 2021 Julio Lopez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
